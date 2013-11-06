f1095cf1d4d97f26525023a4f2fa41de5a3eebb2
Matt Brennan <mattyb1000@gmail.com>
2013-11-06 12:38:26 +0000

    setting value support

--- drag.js
+++ drag.js
@@ -71,7 +71,7 @@ function Range(options) {
     $el.range = range;
     $el.css({
       left: 100*range[0] + '%',
-      width: 100*(range[1] - range[0]) + '%'
+      minWidth: 100*(range[1] - range[0]) + '%'
     });
 
     return $el;
@@ -80,33 +80,28 @@ function Range(options) {
   if(options.value) $el.val(options.value);
 
   $el.on('mousedown', function(ev) {
-    var target;
-
     if ($(ev.target).is('.handle:first-child')) {
-      target = $(ev.currentTarget).width('auto');
       $('body').addClass('resizing');
       $(document).on('mousemove',resizeLeft);
     } else if ($(ev.target).is('.handle:last-child')) {
-      target = $(ev.currentTarget).width('auto');
       $('body').addClass('resizing');
       $(document).on('mousemove',resizeRight);
     } else {
-      target = $(this);
       $('body').addClass('dragging');
       $(document).on('mousemove',drag);
     }
 
-    var startLeft = target.offset().left,
-        startPosLeft = target.position().left,
-        mouseOffset = ev.clientX ? ev.clientX - target.offset().left : 0,
-        startWidth = target.width(),
-        parent = target.parent(),
+    var startLeft = $el.offset().left,
+        startPosLeft = $el.position().left,
+        mouseOffset = ev.clientX ? ev.clientX - $el.offset().left : 0,
+        startWidth = $el.width(),
+        parent = options.parent,
         parentOffset = parent.offset(),
         parentWidth = parent.width();
 
     var nextLeft, prevRight;
 
-    target.siblings('.bar').each(function() {
+    $el.siblings('.bar').each(function() {
       var $this = $(this), off = $this.offset(), width = $this.width();
       if(off.left >= startLeft + startWidth) { // to the right of current bar
         if(!nextLeft || off.left <= nextLeft) { // no next or closer to current than next
@@ -123,9 +118,6 @@ function Range(options) {
     var drawing = false;
 
     $(document).on('mouseup', function() {
-      requestAnimationFrame(function() {
-        target = null;
-      });
       $(this).off('mouseup mousemove');
       $('body').removeClass('resizing dragging');
     });
@@ -139,12 +131,13 @@ function Range(options) {
       if (width >= 10) {
         requestAnimationFrame(function() {
           drawing = false;
-          if(target) target.css('min-width', width);
+          var right = width / parentWidth;
+          $el.val([$el.range[0], $el.range[0] + right]);
         });
         drawing = true;
       } else {
         $(document).trigger('mouseup');
-        target.find('.handle:first-child').trigger('mousedown');
+        $el.find('.handle:first-child').trigger('mousedown');
       }
     }
 
@@ -154,14 +147,15 @@ function Range(options) {
       if (drawing) return;
       if (nextLeft && left + startWidth >= nextLeft) left = nextLeft - startWidth;
       if (prevRight && left <= prevRight) left = prevRight;
-      if (left >= 0 && left <= parentWidth - target.width()) {
+      if (left >= 0 && left <= parentWidth - $el.width()) {
         requestAnimationFrame(function() {
           drawing = false;
-          if(target) target.css('left', left);
+          var rangeOffset = left / parentWidth - $el.range[0];
+          $el.val([left / parentWidth, $el.range[1] + rangeOffset]);
         });
         drawing = true;
       } else {
-        mouseOffset = ev.clientX - target.offset().left;
+        mouseOffset = ev.clientX - $el.offset().left;
       }
     }
 
@@ -181,15 +175,13 @@ function Range(options) {
       if (width >= 10) {
         requestAnimationFrame(function() {
           drawing = false;
-          if(target) target.css({
-            minWidth: width,
-            left: left
-          });
+          var lRel = left / parentWidth;
+          $el.val([lRel, $el.range[1]]);
         });
         drawing = true;
       } else {
         $(document).trigger('mouseup');
-        target.find('.handle:last-child').trigger('mousedown');
+        $el.find('.handle:last-child').trigger('mousedown');
       }
     }
   });
