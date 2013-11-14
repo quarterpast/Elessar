61b15d53e1bed4da80571c4055ed6236bfe93adf
Matt Brennan <mattyb1000@gmail.com>
2013-11-14 13:43:43 +0000

    basic indicator support

---
+++ indicator.js
@@ -0,0 +1,34 @@
+(function(root, definition) {
+  if (typeof define === 'function' && define.amd) {
+    define(['jquery','es5-shim'], definition);
+  } else if (typeof exports === 'object') {
+    module.exports = definition(require('jquery-browser/lib/jquery'), require('es5-shim'));
+  } else {
+    root.Indicator = definition(jQuery);
+  }
+})(this, function($) {
+  function Indicator(options) {
+    var $el = $('<div class="elessar-indicator">');
+
+    if(options.indicatorClass) $el.addClass(options.indicatorClass);
+
+    var drawing = false;
+
+    $el.val = function(pos) {
+      if (drawing) return $el;
+      requestAnimationFrame(function() {
+        drawing = false;
+        $el.css({left: 100*pos + '%'});
+      });
+      drawing = true;
+
+      return $el;
+    };
+
+    if(options.value) $el.val(options.value);
+
+    return $el;
+  }
+
+  return Indicator;
+});
